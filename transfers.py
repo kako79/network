d9e3418413e1a3c15267137b93f985c6cffb4c24
kako79 <kako79@gmail.com>
2018-07-04 10:52:09 +0100

    transfers.py

--- transfers.py
+++ transfers.py
@@ -4,7 +4,7 @@ import datetime
 
 def get_separate_date_time(datetimeentry):
     print(datetimeentry)
-    if datetimeentry == ' ':
+    if datetimeentry == 'nan':
         return datetime.datetime.max
     else:
         #this returns the date in a format where the hours and days can be accessed eg d.year or d.minute
