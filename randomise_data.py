d960a82fed08ddb751dc1539b13f5bd8e06a04bd
kako79 <kako79@gmail.com>
2018-07-16 11:31:57 +0100

    export graph weights

--- randomise_data.py
+++ randomise_data.py
@@ -20,7 +20,7 @@ import datetime
 alldata= pd.read_csv("combined_data.csv")
 
 alldata['effective_time'] = pd.to_datetime(alldata['effective_time'], format = "%Y-%m-%d %H:%M:%S")
-alldata['discharge_time'] = pd.to_datetime(alldata['discharge_time'], format = "%Y-%m-%d %H:%M:%S")
+#alldata['discharge_time'] = pd.to_datetime(alldata['discharge_time'], format = "%Y-%m-%d %H:%M:%S")
 alldata['admission_time'] = pd.to_datetime(alldata['admission_time'], format = "%Y-%m-%d %H:%M:%S")
 alldata['transfer_time'] = pd.to_datetime(alldata['transfer_time'], format = "%Y-%m-%d %H:%M:%S")
 
@@ -30,7 +30,7 @@ alldata['transfer_time'] = pd.to_datetime(alldata['transfer_time'], format = "%Y
 #tt = alldata['transfer_time']+ datetime.timedelta(days = 5)
 
 alldata['effective_time'] = alldata['effective_time'].map(lambda d: d + datetime.timedelta(days=5))
-alldata['discharge_time'] = alldata['discharge_time'].map(lambda d: d + datetime.timedelta(days=5))
+#alldata['discharge_time'] = alldata['discharge_time'].map(lambda d: d + datetime.timedelta(days=5))
 alldata['admission_time'] = alldata['admission_time'].map(lambda d: d + datetime.timedelta(days=5))
 alldata['transfer_time'] = alldata['transfer_time'].map(lambda d: d + datetime.timedelta(days=5))
 alldata = alldata.drop(['specialty'], axis=1)
