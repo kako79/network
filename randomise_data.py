53534fffadb45824a14c9ed93a2d6d2a0011037a
kako79 <kako79@gmail.com>
2018-07-16 11:18:27 +0100

    export graph weights

---
+++ randomise_data.py
@@ -0,0 +1,27 @@
+import numpy as np
+import itertools
+import functools
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+import pandas as pd
+#from mpl_toolkits import mplot3d
+#from matplotlib import cm
+#from matplotlib import colors
+import networkx as nx
+#from collections import Counter
+#from itertools import chain
+#from collections import defaultdict
+from datetime import datetime
+
+alldata= pd.read_csv("combined_data.csv")
+
+et = alldata['effective_time'] + datetime.timedelta(days=5)
+dt = alldata['discharge_time'] + datetime.timedelta(days = 5)
+at = alldata['admission_time']+ datetime.timedelta(days = 5)
+tt = alldata['transfer_time']+ datetime.timedelta(days = 5)
+
+alldata.to_csv('offset_data.csv', header=True, index=False)
+
