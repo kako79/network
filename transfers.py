1fbc291b063ccfcc955919b54e864ff8b6767b81
kako79 <kako79@gmail.com>
2018-08-15 13:42:27 +0100

    export graph weights

--- transfers.py
+++ transfers.py
@@ -82,6 +82,13 @@ full_info = full_info.join(surg_extra, on='ptid', how='left')
 
 full_info = full_info[full_info.adt_department_name != ''] # removes all lines where there is no location given
 full_info = full_info[full_info.adt_department_name != ' '] # removes all lines where there is no location given
+
+#remove any lines where the location is not at least 3 length
+full_info['adt_department_name'] = full_info['adt_department_name'].astype('str')
+mask = (full_info['adt_department_name'].str.len() >= 5)
+full_info = full_info.loc[mask]
+
+
 full_info.to_csv('full_info_all.csv', header=True, index=False)
 
 print('full_info created')
