28064005fcfa8af4281cb4fd808ccc4bd8633a2b
kako79 <kako79@gmail.com>
2018-08-16 12:50:18 +0100

    export graph weights

--- model_system.py
+++ model_system.py
@@ -6,9 +6,16 @@ from collections import deque, namedtuple
 alltransfers = pd.read_csv("all_transfers_file.csv")
 
 alltransfers.rename(index=str, columns={'from': 'from_loc'}, inplace=True)
-list_wards = alltransfers.from_loc.unique()
-
+alltransfers.rename(index=str, columns={'from': 'from_loc'}, inplace=True)
+list_from_wards = alltransfers.from_loc.unique()
+list_to_wards = alltransfers.to_loc.unique()
 #need to make an assignment of the individual wards to a category eg surgical ward, outlier, high dependency...
+
+list_from_wards.to_csv('list of wards.csv')
+
+
+ward_dictionary = {'ADD '}
 
 
 print(list_wards)
+
