99421f215b8b415933dd1819ff5af9e5b1ce8dd4
kako79 <kako79@gmail.com>
2018-10-12 15:04:47 +0100

    weekday admissions

--- analysis_network_data.py
+++ analysis_network_data.py
@@ -173,14 +173,14 @@ weekday_transfers = alldata[~alldata['is_weekend']]
 #specific_data.loc[admpoint[specific_data['admission_time'] == specific_data['extraid']].index, 'to'] = 'discharge'
 #weekend_trans_by_month = [weekend_transfer[weekend_transfer['month'] == i] for i in range(1, 13)]
 
-monthlist=[1,2]
-#monthlist=[1,2,3,4,5,6,7,8,9,10,11,12]
+#monthlist=[1,2]
+monthlist=[1,2,3,4,5,6,7,8,9,10,11,12]
 number_list = []
 degrees_list = []
 for i in monthlist:
     month_data = weekend_transfers[weekend_transfers['transfer_month'] == i]
     # drop the columns that are not needed for the graph, also select adults or children
-    month_data_reduced = month_data.loc[month_data['age'] < 16].drop(['transfer_dt', 'dt_adm', 'dt_dis', 'spec', 'age', 'asa'], axis=1)
+    month_data_reduced = month_data.loc[month_data['age'] > 16].drop(['transfer_dt', 'dt_adm', 'dt_dis', 'spec', 'age', 'asa'], axis=1)
     get_network_analytics(month_data_reduced)
     print(i)
 
