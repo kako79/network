9fb41fd3dc130a3ce82225c8f424c27e3894a576
kako79 <kako79@gmail.com>
2018-10-18 14:59:50 +0100

    weekday admissions

---
+++ add_on_performance_data.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import datetime
+import numpy as np
+from collections import deque, namedtuple
+
+def get_separate_date_time(datetimeentry):
+    print(datetimeentry)
+    if type(datetimeentry) == float:
+        return datetime.datetime.max
+    else:
+        #this returns the date in a format where the hours and days can be accessed eg d.year or d.minute
+        separate_date_time = datetime.datetime.strptime(datetimeentry,"%Y-%m-%d %H:%M:%S")
+        return separate_date_time
+
+
+def get_date_only(date_time_entry):
+    separated_date_entry = get_separate_date_time(date_time_entry)
+    print(separated_date_entry)
+    year_only = separated_date_entry.year
+    month_only = separated_date_entry.month
+    day_only = separated_date_entry.day
+    date_only = datetime.datetime(year_only, month_only, day_only)
+    return date_only.date()
+
+
+
+
+
+
+
+all_transfers = pd.read_csv("all_transfers_1110.csv")
+
+#add on the information about the hospital state from the ED performance file
+ed_performance = pd.read_csv("ed_perfomance_with_average.csv")
+# need transfer date only in a separate column
+all_transfers['transfer_date_only'] = get_date_only(all_transfers['transfer_dt'])
+ed_performance.set_index('date', drop = True, inplace = True)
+all_transfers_with_performance = ed_performance.join(all_transfers, on='transfer_date_only', how='left')
+
+
+
+all_transfers.to_csv('all_transfers_1110.csv', header=True, index=False)
+print('transfers file created')
+##!!! finish of creating the transfers file
