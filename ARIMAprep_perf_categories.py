1eb7c1ca846f4c03186990163a171f1cde603057
kako79 <kako79@gmail.com>
2018-10-31 11:18:47 +0000

    weekday admissions

--- ARIMAprep_perf_categories.py
+++ ARIMAprep_perf_categories.py
@@ -58,7 +58,7 @@ print("Rows after removing bad dates: %s" % len(all_transfers))
 #add on the information about the hospital state from the ED performance file
 ed_performance = pd.read_csv("ed_performance_all.csv")
 # need transfer date only in a separate column
-ed_performance['date'] = pd.to_datetime(ed_performance['day'], format='%Y-%m-%d')
+ed_performance['date'] = pd.to_datetime(ed_performance['day'], format='%d/%m/%Y')
 ed_performance['date_number'] = ed_performance['date'].map(get_date_number)
 ed_performance.drop(['date'], axis=1, inplace=True)
 ed_performance.set_index('date_number', drop=True, inplace=True)
@@ -66,7 +66,7 @@ all_transfers_with_edperf = all_transfers.join(ed_performance, on='transfer_date
 
 #add on bedstate information - all beds
 bedstate_info = pd.read_csv("all_beds_info.csv")
-bedstate_info['date'] = pd.to_datetime(bedstate_info['Date'], format='%d/%m/%Y')
+bedstate_info['date'] = pd.to_datetime(bedstate_info['Date'], format='%Y-%m-%d')
 bedstate_info['date_number'] = bedstate_info['date'].map(get_date_number)
 bedstate_info.drop(['date'], axis = 1, inplace = True)
 bedstate_info.set_index('date_number', drop = True, inplace = True)
