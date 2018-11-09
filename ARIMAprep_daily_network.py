9387920464366ce61a7afc732ea3564b4e9e9888
kako79 <kako79@gmail.com>
2018-11-09 11:01:24 +0000

    weekday admissions

--- ARIMAprep_daily_network.py
+++ ARIMAprep_daily_network.py
@@ -171,7 +171,7 @@ def get_network_analytics(data_reduced):
         average_degree_connectivity_ae = 0
         average_degree_connectivity_theatre = 0
     else:
-        average_degree_connectivity = nx.average_degree_connectivity(G,source="out+in", target="out+in",weight = 'weight')
+        average_degree_connectivity = nx.average_degree_connectivity(G,source="in+out", target="in+out",weight = 'weight')
         if 'AE' in average_degree_connectivity:
             average_degree_connectivity_ae = average_degree_connectivity['AE']
         else:
