cc5522c0630ca478cde05e3fe2305cb50b835edb
kako79 <kako79@gmail.com>
2018-11-09 11:14:27 +0000

    weekday admissions

--- ARIMAprep_daily_network.py
+++ ARIMAprep_daily_network.py
@@ -125,7 +125,7 @@ def get_network_analytics(data_reduced):
     med_to_surg = G.get_edge_data('general medical ward', 'general surgical ward', default ={}).get('weight', 0)
     med_to_ortho = G.get_edge_data('general medical ward', ' orthopaedic ward', default ={}).get('weight', 0)
     med_to_surg_acute = G.get_edge_data('acute medical ward', 'general surgical ward', default={}).get('weight', 0)
-    med_to_orth_acute = G.get_edge_data(acute medical ward', ' orthopaedic ward', default={}).get('weight', 0)
+    med_to_orth_acute = G.get_edge_data('acute medical ward', ' orthopaedic ward', default={}).get('weight', 0)
 
 
     total_medical_ward_transfers = med_to_med_acute + med_to_med_general+med_to_med_acgen+med_to_med_genac+ med_to_ortho+ med_to_surg+ med_to_surg_acute+ med_to_orth_acute
