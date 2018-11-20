5d1fab494431651692d0b79c1ce7ae4e0986cd9f
kako79 <kako79@gmail.com>
2018-11-20 13:50:42 +0000

    weekday admissions

--- ARIMAprep_daily_network.py
+++ ARIMAprep_daily_network.py
@@ -184,7 +184,7 @@ def get_network_analytics(data_reduced):
 
 
     for c in nx.connected_component_subgraphs(G):
-        shortest_path = nx.average_shortest_path_length(c))
+        shortest_path = nx.average_shortest_path_length(c)
     #diameter_net = nx.diameter(G)
     diameter_net = 0
     #radius_net = nx.radius(G)
