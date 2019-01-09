db4c6932c97e922744f6da7b670c54d8acfab7f1
kako79 <kako79@gmail.com>
2019-01-09 17:29:07 +0000

    Don't save bad patient data if there isn't any.

--- add_strain_to_transfer_file.py
+++ add_strain_to_transfer_file.py
@@ -55,7 +55,7 @@ bedstate_info.set_index('date_number', drop = True, inplace = True)
 transfer_data_beded= transfer_data_ed.join(bedstate_info, on = 'date_as_number', how = 'left')
 
 transfer_data_beded['bedsfree'] = transfer_data_beded['Total Occupied'].map(get_free_beds)
-transfer_data_beded['strain'] = transfer_data_beded.bedsfree * transfer_data_beded.breach_percentage
+transfer_data_beded['strain'] = 1.0/transfer_data_beded.bedsfree * transfer_data_beded.breach_percentage *100
 transfer_strain = transfer_data_beded.drop(['date_as_number','date', 'day', 'Total Occupied'], axis=1)
 
 
