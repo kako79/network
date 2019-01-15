fcf266b5d6e330b63607a87e7590dd396580d53a
kako79 <kako79@gmail.com>
2019-01-15 12:20:20 +0000

    Don't save bad patient data if there isn't any.

--- make_graph.py
+++ make_graph.py
@@ -684,7 +684,7 @@ minimal_cat_ward_dict = {'ADD A3 WARD': 'neurosurgery ward', 'ADD A4 WARD': 'neu
 
 
 #read in the data from a combined csv file
-alldata = pd.read_csv("transfers_paeds_all.csv")
+alldata = pd.read_csv("transfers_old_to_1301.csv")
 #alldata= pd.read_csv("transfers_old_t_o.csv")
 location_category_map = nocat_ward_weekday
 
@@ -929,7 +929,8 @@ all_network_info_df = pd.DataFrame(columns=['sum of transfers','number nodes', '
                                          'incentrality theatres', 'outcentrality theatres', 'bet centrality theatres','medical to theatre','medical ward transfers',
                                          'med surg ratio','eigen_centr_theatre','eigen_centr_ed', 'density', 'transitivity', 'clustering average', 'average shortest path'], data = data_list)
 
-filename = '_nocatwd_1501_paeds'
+filename = '_nocatwd_1501_toold'
+
 
 all_network_info_df.to_csv('info' + filename + '.csv', header=True, index=False)
 edge_weight_data.to_csv('edge' + filename + '.csv', header=True, index=False)
