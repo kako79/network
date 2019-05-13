ae249d5c5744b2052d08774d6ea1d3394a3ad6f3
kako79 <kako79@gmail.com>
2019-05-13 16:19:17 +0100

    Don't save bad patient data if there isn't any. AE and admission combined

--- demographics.py
+++ demographics.py
@@ -33,18 +33,18 @@ num_good_patients = 0
 good_patient_data =[]
 bad_patient_data = []
 
-for ptid, group in groups:
-    #patient_transfers, patient_data = get_patient_transfers(ptid, group)
-    if patient_transfers is None:
-        num_bad_patients += 1
-        if patient_data is not None:
-            bad_patient_data.append(patient_data)
-    else:
-        num_good_patients += 1
-        good_patient_data.append(patient_transfers)
-    i += 1
-    if (i % 100) == 0:
-        print("Finished %s of %s patients. Good patients: %s, bad patients: %s." % (i, num_patients, num_good_patients, num_bad_patients))
+#for ptid, group in groups:
+#    #patient_transfers, patient_data = get_patient_transfers(ptid, group)
+#    if patient_transfers is None:
+ #       num_bad_patients += 1
+#        if patient_data is not None:
+#            bad_patient_data.append(patient_data)
+#    else:
+#        num_good_patients += 1
+#        good_patient_data.append(patient_transfers)
+#    i += 1
+#    if (i % 100) == 0:
+#        print("Finished %s of %s patients. Good patients: %s, bad patients: %s." % (i, num_patients, num_good_patients, num_bad_patients))
 
 print("Good patients: %s. Bad patients: %s." % (num_good_patients, num_bad_patients))
 
