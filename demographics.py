6d7d1d610cad749feebb9f6e5fe76160301d267c
kako79 <kako79@gmail.com>
2019-07-29 16:32:01 +0100

    demographics

--- demographics.py
+++ demographics.py
@@ -23,8 +23,8 @@ allpatients = pd.read_csv("ADM_INFO_aug.csv")
 
 reduced_data = allpatients[['STUDY_SUBJECT_DIGEST','adm_hosp','dis_hosp','specialty','admAge']]
 reduced_data.rename(index=str, columns={'STUDY_SUBJECT_DIGEST': 'ptid'}, inplace=True)
-
-
+reduced_data =  reduced_data.loc[reduced_data['admAge']>16]
+print("all admissions", len(reduced_data['admAge']))
 print(reduced_data['specialty'].value_counts())
 
 
