b039dc7d781b43ca674e8c91594c234eb5fc95f8
kako79 <kako79@gmail.com>
2019-09-13 15:04:19 +0100

    demographics

--- ARIMAprep_perf_categories.py
+++ ARIMAprep_perf_categories.py
@@ -54,7 +54,7 @@ def get_transfer_day(date):
 all_transfers = pd.read_csv("transfers_all_icu.csv")
 all_transfers['transfer_dt'] = all_transfers['transfer_dt'].map(get_separate_date_time)
 all_transfers['transfer_date_number'] = all_transfers['transfer_dt'].map(get_date_number)
-
+print(min(all_transfers['transfer_date_number']))
 
 print("Rows: %s" % len(all_transfers))
 
