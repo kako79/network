4eb8bd3072e807b1fd133281ad7ffc42517c1709
kako79 <kako79@gmail.com>
2019-09-22 15:25:03 +0100

    demographics

--- select_patients_from_transfers.py
+++ select_patients_from_transfers.py
@@ -119,29 +119,29 @@ adult_transfers.to_csv('all_adult_transfers.csv')
 
 #select all the patients who at some point in their stay were in icu, nccu
 #df = pd.DataFrame({'ptid': [1, 1, 1, 2, 2, 3, 3, 3, 3, 4], 'loc': ['a', 'b', 'c', 'a', 'c', 'a', 'b', 'a', 'b', 'd']})
-wards = {'ADD GENERAL ICU', 'ADD NEURO ICU', }
-icu_patient_ids = set(adult_transfers.loc[adult_transfers['from'].isin(wards)]['ptid'].unique())
-icu_patient_records = adult_transfers.loc[adult_transfers['ptid'].isin(icu_patient_ids)]
-icu_patient_records.to_csv('transfers_all_icu.csv', header=True, index=False)
+#wards = {'ADD GENERAL ICU', 'ADD NEURO ICU', }
+#icu_patient_ids = set(adult_transfers.loc[adult_transfers['from'].isin(wards)]['ptid'].unique())
+#icu_patient_records = adult_transfers.loc[adult_transfers['ptid'].isin(icu_patient_ids)]
+#icu_patient_records.to_csv('transfers_all_icu.csv', header=True, index=False)
 
 #all adult HDU or ICU patients
-wards = {'ADD GENERAL ICU', 'ADD NEURO ICU', 'ADD D4 IDA UNIT', 'ADD CORONARY CARE UNIT', 'ADD TRANSPLANT HDU'}
-icu_patient_ids = set(adult_transfers.loc[adult_transfers['from'].isin(wards)]['ptid'].unique())
-icu_patient_records = adult_transfers.loc[adult_transfers['ptid'].isin(icu_patient_ids)]
-icu_patient_records.to_csv('transfers_icu.csv', header=True, index=False)
+#wards = {'ADD GENERAL ICU', 'ADD NEURO ICU', 'ADD D4 IDA UNIT', 'ADD CORONARY CARE UNIT', 'ADD TRANSPLANT HDU'}
+#icu_patient_ids = set(adult_transfers.loc[adult_transfers['from'].isin(wards)]['ptid'].unique())
+#icu_patient_records = adult_transfers.loc[adult_transfers['ptid'].isin(icu_patient_ids)]
+#icu_patient_records.to_csv('transfers_icu.csv', header=True, index=False)
 
 #select the patients who got to HDU but on busy and non busy days
 #busy
-wards = {'ADD GENERAL ICU', 'ADD NEURO ICU', 'ADD D4 IDA UNIT', 'ADD CORONARY CARE UNIT', 'ADD TRANSPLANT HDU'}
-icu_patient_ids = set(transfers_around_low_ed_ind.loc[transfers_around_low_ed_ind['from'].isin(wards)]['ptid'].unique())
-icu_patient_records = transfers_around_low_ed_ind.loc[transfers_around_low_ed_ind['ptid'].isin(icu_patient_ids)]
-icu_patient_records.to_csv('transfers_lowed_hdu.csv', header=True, index=False)
+#wards = {'ADD GENERAL ICU', 'ADD NEURO ICU', 'ADD D4 IDA UNIT', 'ADD CORONARY CARE UNIT', 'ADD TRANSPLANT HDU'}
+#icu_patient_ids = set(transfers_around_low_ed_ind.loc[transfers_around_low_ed_ind['from'].isin(wards)]['ptid'].unique())
+#icu_patient_records = transfers_around_low_ed_ind.loc[transfers_around_low_ed_ind['ptid'].isin(icu_patient_ids)]
+#icu_patient_records.to_csv('transfers_lowed_hdu.csv', header=True, index=False)
 
 #calm ED
-wards = {'ADD GENERAL ICU', 'ADD NEURO ICU', 'ADD D4 IDA UNIT', 'ADD CORONARY CARE UNIT', 'ADD TRANSPLANT HDU'}
-icu_patient_ids = set(transfers_around_high_ed_ind.loc[transfers_around_high_ed_ind['from'].isin(wards)]['ptid'].unique())
-icu_patient_records = transfers_around_high_ed_ind.loc[transfers_around_high_ed_ind['ptid'].isin(icu_patient_ids)]
-icu_patient_records.to_csv('transfers_highed_hdu.csv', header=True, index=False)
+#wards = {'ADD GENERAL ICU', 'ADD NEURO ICU', 'ADD D4 IDA UNIT', 'ADD CORONARY CARE UNIT', 'ADD TRANSPLANT HDU'}
+#icu_patient_ids = set(transfers_around_high_ed_ind.loc[transfers_around_high_ed_ind['from'].isin(wards)]['ptid'].unique())
+#icu_patient_records = transfers_around_high_ed_ind.loc[transfers_around_high_ed_ind['ptid'].isin(icu_patient_ids)]
+#icu_patient_records.to_csv('transfers_highed_hdu.csv', header=True, index=False)
 
 
 
@@ -156,19 +156,19 @@ trauma_records = alltransfers.loc[alltransfers['ptid'].isin(trauma_ids)]
 trauma_adult_records = trauma_records.loc[trauma_records['age'] >16]
 trauma_adult_records.to_csv('transfers_trauma_adult.csv', header = True, index = False)
 
-age_old = {'80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95'}
-t_o_old_patient_ids = set(t_o_patient_records.loc[t_o_patient_records['age'].isin(age_old)]['ptid'].unique())
-t_o_old_patient_records = t_o_patient_records.loc[t_o_patient_records['ptid'].isin(t_o_old_patient_ids)]
-t_o_old_patient_records.to_csv('transfers_old_tando.csv', header=True, index=False)
+#age_old = {'80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95'}
+#t_o_old_patient_ids = set(t_o_patient_records.loc[t_o_patient_records['age'].isin(age_old)]['ptid'].unique())
+#t_o_old_patient_records = t_o_patient_records.loc[t_o_patient_records['ptid'].isin(t_o_old_patient_ids)]
+#t_o_old_patient_records.to_csv('transfers_old_tando.csv', header=True, index=False)
 
 #ASA 3 and 4 adult patients
-asacategory= {'3','4'}
-asa34_adult_ids = set(adult_transfers.loc[adult_transfers['asa'].isin(asacategory)]['ptid'].unique())
-asa34_adult_records = adult_transfers.loc[adult_transfers['ptid'].isin(asa34_adult_ids)]
-asa34_adult_records.to_csv('transfers_adult_asa34.csv', header=True, index=False)
+#asacategory= {'3','4'}
+#asa34_adult_ids = set(adult_transfers.loc[adult_transfers['asa'].isin(asacategory)]['ptid'].unique())
+#asa34_adult_records = adult_transfers.loc[adult_transfers['ptid'].isin(asa34_adult_ids)]
+#asa34_adult_records.to_csv('transfers_adult_asa34.csv', header=True, index=False)
 
 #paeds patients
-paeds_transfers = alltransfers.loc[alltransfers['age'] < 16]
-paeds_transfers.to_csv('transfers_paeds_all.csv', header=True, index=False)
+#paeds_transfers = alltransfers.loc[alltransfers['age'] < 16]
+#paeds_transfers.to_csv('transfers_paeds_all.csv', header=True, index=False)
 
 
