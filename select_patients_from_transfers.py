753cea475c84c8aa6e63c6e2525ac5bc4a029c16
kako79 <kako79@gmail.com>
2019-09-23 11:11:33 +0100

    demographics

--- select_patients_from_transfers.py
+++ select_patients_from_transfers.py
@@ -69,7 +69,7 @@ adult_transfers= alltransfers.loc[alltransfers['age']>16]
 adult_transfers.to_csv('all_adult_transfers.csv')
 #select transfers on specific dates with low breach percentage ie days where A&E was very full
 transfers_lowed = alltransfers[alltransfers['breach_percentage'] < 0.6955]
-transfers_lowed = adult_transfers[adult_transfers['breach_percentage'] < 0.6955]
+#transfers_lowed = adult_transfers[adult_transfers['breach_percentage'] < 0.6955]
 #transfers_lowed.to_csv('transfers_lowedpercentage.csv')
 
 #select patients for the day before, the day of and the day after a full A&E
@@ -93,13 +93,13 @@ for i in low_ed_perc_dates:
 all_dates_low_ed = set(low_ed_prev_day + list(low_ed_perc_dates) + low_ed_next_day)
 alltransfers['day_of_transfer'] = alltransfers['transfer_dt'].map(get_transfer_day)
 #print(alltransfers['day_of_transfer'])
-transfers_around_low_ed_ind = adult_transfers[adult_transfers['day_of_transfer'].isin(all_dates_low_ed)]
+transfers_around_low_ed_ind = alltransfers[alltransfers['day_of_transfer'].isin(all_dates_low_ed)]
 print(len(transfers_around_low_ed_ind))
 transfers_around_low_ed_ind.to_csv('transfers_around_low_ed_perc.csv')
 
 #select transfers on specific dates with low breach percentage ie days where A&E was very empty
 transfers_highed = alltransfers[alltransfers['breach_percentage'] >0.9685]
-transfers_highed = adult_transfers[adult_transfers['breach_percentage'] >0.9685]
+#transfers_highed = adult_transfers[adult_transfers['breach_percentage'] >0.9685]
 #transfers_lowed.to_csv('transfers_lowedpercentage.csv')
 
 #select patients for the day before, the day of and the day after a full A&E
@@ -117,7 +117,7 @@ for i in high_ed_perc_dates:
 #import pdb; pdb.set_trace()
 all_dates_high_ed = set(high_ed_prev_day + list(high_ed_perc_dates) + high_ed_next_day)
 #print(alltransfers['day_of_transfer'])
-transfers_around_high_ed_ind = adult_transfers[adult_transfers['day_of_transfer'].isin(all_dates_high_ed)]
+transfers_around_high_ed_ind = alltransfers[alltransfers['day_of_transfer'].isin(all_dates_high_ed)]
 print(len(transfers_around_high_ed_ind))
 transfers_around_high_ed_ind.to_csv('transfers_around_high_ed_perc.csv')
 
