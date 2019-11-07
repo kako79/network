e6a327cc96179b0e51e8459557a69b9fb730eb48
kako79 <kako79@gmail.com>
2019-11-07 12:18:20 +0000

    journeys module started

--- make_journeys.py
+++ make_journeys.py
@@ -21,39 +21,65 @@ transfers = transfers.drop(['from','to'], axis=1) # we dont need these anymore
 
 COLUMN_NAMES = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
                 '20', '21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40']
-maxLen = len(COLUMN_NAMES)
-groups = transfers.groupby('ptid')
+#maxLen = len(COLUMN_NAMES)
+#groups = transfers.groupby('ptid')
 
-df_journeys = pd.DataFrame(columns=COLUMN_NAMES)
+#df_journeys = pd.DataFrame(columns=COLUMN_NAMES)
 
 locations_not_needed = ['XR', 'clinic', 'PET', 'IR', 'CT']
-longest_journey = maxLen
+#longest_journey = maxLen
 
-for ptid, group in groups:
-    journey = []
-    numbers = []
-    for row_index, row in group.iterrows():
-        row_location = row['from_cat']
-        if row_location in locations_not_needed:
-            continue
-        if row_location in journey:
-            # print('here')
-            number_loc = journey.count(row_location) + 1
-            # print(number_loc)
-        else:
-            # print('second here')
-            number_loc = 1
-        numbers.append(number_loc)
-        journey.append(row['from_cat'])
-        if len(journey) > longest_journey:
-            #print('long journey', len(journey))
-            longest_journey = len(journey)
-            print(longest_journey)
 
-    journey = journey + [0] * (maxLen - len(journey))
-    numbers = numbers + [0] * (maxLen - len(numbers))
-    full = [str(i) + str(j) for i, j in zip(journey, numbers)]
-    df_journeys.append(full)
+def get_patient_journey(ptid, group):
+    counts = dict()
+    journey_row = {'ptid': ptid}
+
+    for i, row in group.iterrows():
+        loc = row['from_cat']
+        if not (loc in locations_not_needed):
+            num = counts.get(loc, 1)
+            counts[loc] = num + 1
+            loc_name = f"{loc}{num}"
+            column_name = f"loc_{str(len(journey_row)).rjust(2, '0')}"
+            journey_row[column_name] = loc_name
+
+    return journey_row
+
+
+journey_data = [get_patient_journey(ptid, group) for ptid, group in transfers.groupby('ptid')]
+
+df_journeys = pd.DataFrame(data=journey_data).fillna('dc')
+
+
+
+
+
+#
+#for ptid, group in groups:
+#    journey = []
+#    numbers = []
+#    for row_index, row in group.iterrows():
+#        row_location = row['from_cat']
+#        if row_location in locations_not_needed:
+#            continue
+#        if row_location in journey:
+#            # print('here')
+#            number_loc = journey.count(row_location) + 1
+#            # print(number_loc)
+#        else:
+#            # print('second here')
+#            number_loc = 1
+#        numbers.append(number_loc)
+#        journey.append(row['from_cat'])
+#        if len(journey) > longest_journey:
+#            #print('long journey', len(journey))
+#            longest_journey = len(journey)
+#            print(longest_journey)
+#
+#    journey = journey + [0] * (maxLen - len(journey))
+#    numbers = numbers + [0] * (maxLen - len(numbers))
+#    full = [str(i) + str(j) for i, j in zip(journey, numbers)]
+#    df_journeys.append(full)
     #print(full)
 
 df_journeys.to_csv('journeys_plain.csv', header = True, index = True)
