194923ff2d5af721246866bb622801d373ff7fae
kako79 <kako79@gmail.com>
2019-12-11 14:00:15 +0000

    journeys module started

---
+++ accumulation_for_timos.py
@@ -0,0 +1,18 @@
+import pandas as pd
+#from mpl_toolkits import mplot3d
+#from matplotlib import cm
+#from matplotlib import colors
+import networkx as nx
+#from collections import Counter
+#from itertools import chain
+#from collections import defaultdict
+from datetime import datetime
+import datetime as dt
+
+#file containing all the categorised and uncategorised locations
+transfers = pd.read_csv("cat_data_journeys.csv")
+
+pivotdf = transfers.pivot_table(index='ptid', columns='from', aggfunc=len)
+
+pivotdf.to_csv('pivotdf', header = True, index = True)
+
